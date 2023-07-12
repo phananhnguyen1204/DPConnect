d755a4d28a993361fa734fdac69a6632db587164
Phan Anh <phananh12042003@gmail.com>
2023-07-12 19:16:07 -0400

    set up middlewares 🚀

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,51 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+//dot config
+dotenv.config();
 
 //create express app
 const app = express();
+
+//Morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+//Helmet
+app.use(helmet());
+
+//parse json request body
+app.use(express.json());
+
+//parse json request url
+app.use(express.urlencoded({ extended: true }));
+
+//sanitize request data
+app.use(mongoSanitize());
+
+//Enable cookieParser
+app.use(cookieParser());
+
+//gzip compression
+app.use(compression());
+
+//file upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+//cors
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello from server");
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,8 @@
 import app from "./app.js";
-import dotenv from "dotenv";
-
-//dot config
-dotenv.config();
 
 //env variables
 const PORT = process.env.PORT || 8000;
+// console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
   console.log(`Server is listening at ${PORT}...`);
