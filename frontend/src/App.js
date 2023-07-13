b13dfe517f29e233918ee38aede40eef4c2e2b96
Phan Anh <phananh12042003@gmail.com>
2023-07-12 23:54:28 -0400

    user schema

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import { CallIcon, ChatIcon, DocumentIcon } from "./svg";
+// import { CallIcon, ChatIcon, DocumentIcon } from "./svg";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 function App() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => ({ ...state }));
+  console.log(user);
   return (
-    <div>
-      <CallIcon className="fill-blue-600"></CallIcon>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
