b94ce45406adf865112db9cbb1a3a580f504b1e1
Phan Anh <phananh12042003@gmail.com>
2023-07-12 20:44:35 -0400

    setup frontend 🚀

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import { CallIcon, ChatIcon, DocumentIcon } from "./svg";
+function App() {
+  return (
+    <div>
+      <CallIcon className="fill-blue-600"></CallIcon>
+    </div>
+  );
+}
+
+export default App;
