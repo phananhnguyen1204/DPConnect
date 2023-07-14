3e76ea22e801b83d8ddf30115297e9aa4a1ebf2b
Phan Anh <phananh12042003@gmail.com>
2023-07-14 14:21:42 -0400

    conversation model 🚀

---
+++ backend/src/models/conversationModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const conversationSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Conversation is required."],
+      trim: true,
+    },
+    isGroup: {
+      type: Boolean,
+      required: Boolean,
+      default: false,
+    },
+    users: [{ type: ObjectId, ref: "UserModel" }],
+    lastestMessage: {
+      type: ObjectId,
+      ref: "MessageModel",
+    },
+    admin: {
+      type: ObjectId,
+      ref: "UserModel",
+    },
+  },
+  {
+    collection: "conversations",
+    timestamps: true,
+  }
+);
+
+const ConversationModel =
+  mongoose.models.ConversationModel ||
+  mongoose.model("ConversationModel", conversationSchema);
+export default ConversationModel;
