295237961ea6341e48d801b954e24d616194257d
Phan Anh <phananh12042003@gmail.com>
2023-07-14 23:13:13 -0400

    get conversations 🚀

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -40,7 +40,7 @@ export const register = async (req, res, next) => {
         email: newUser.email,
         picture: newUser.picture,
         status: newUser.status,
-        access_token,
+        token: access_token,
       },
     });
   } catch (error) {
@@ -80,7 +80,7 @@ export const login = async (req, res, next) => {
         email: user.email,
         picture: user.picture,
         status: user.status,
-        access_token,
+        token: access_token,
       },
     });
   } catch (error) {
@@ -127,7 +127,7 @@ export const refreshToken = async (req, res, next) => {
         email: user.email,
         picture: user.picture,
         status: user.status,
-        access_token,
+        token: access_token,
       },
     });
   } catch (error) {
