8e9ff92b57c66bab22751a91201004c3cdf61b83
Phan Anh <phananh12042003@gmail.com>
2023-07-15 16:19:29 -0400

    protected route + logout 🚀

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,27 @@
 // import { CallIcon, ChatIcon, DocumentIcon } from "./svg";
 import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
 function App() {
   // const dispatch = useDispatch();
-  const { user } = useSelector((state) => ({ ...state }));
+  const { user } = useSelector((state) => state.user);
+  const { token } = user;
 
   return (
     <div className="dark">
       <Router>
         <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
+          <Route
+            path="/"
+            element={token ? <Home></Home> : <Navigate to="/login"></Navigate>}
+          ></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/register" element={<Register></Register>}></Route>
         </Routes>
--- frontend/src/components/sidebar/header/Menu.jsx
+++ frontend/src/components/sidebar/header/Menu.jsx
@@ -1,4 +1,7 @@
+import { useDispatch } from "react-redux";
+import { logout } from "../../../features/userSlice";
 function Menu() {
+  const dispatch = useDispatch();
   return (
     <div className="absolute right-1 z-50 dark:bg-dark_bg_2 dark:text-dark_text_1 shadow-md w-52">
       <ul>
@@ -14,7 +17,10 @@ function Menu() {
         <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
           <span>Settings</span>
         </li>
-        <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
+        <li
+          className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3"
+          onClick={() => dispatch(logout())}
+        >
           <span>Logout</span>
         </li>
       </ul>
