53dc492c4f5ac927d63009e4dbebcf7c6f55513a
Phan Anh <phananh12042003@gmail.com>
2023-07-14 22:22:06 -0400

    sidebar noti 🚀

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,13 @@
 import SidebarHeader from "./header/SidebarHeader";
+import { Notifications } from "./notifications";
 
 function Sidebar() {
   return (
     <div className="w-[40%] h-full select-none">
       {/* Sidebar header */}
       <SidebarHeader></SidebarHeader>
+      {/* Notification */}
+      <Notifications></Notifications>
     </div>
   );
 }
