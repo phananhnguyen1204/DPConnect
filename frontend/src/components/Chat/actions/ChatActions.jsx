17b03a65867d60adc1010b1392618e0a40524c22
Phan Anh <phananh12042003@gmail.com>
2023-07-15 22:46:55 -0400

    send a message function 🚀

--- frontend/src/components/Chat/actions/ChatActions.jsx
+++ frontend/src/components/Chat/actions/ChatActions.jsx
@@ -1,11 +1,35 @@
 import { SendIcon } from "../../../svg";
+import { ClipLoader } from "react-spinners";
 import Attachments from "./Attachments";
 import EmojiPicker from "./EmojiPicker";
 import Input from "./Input";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { sendMessage } from "../../../features/chatSlice";
 
 function ChatActions() {
+  const dispatch = useDispatch();
+  const { activeConversation, status } = useSelector((state) => state.chat);
+  const { user } = useSelector((state) => state.user);
+  const { token } = user;
+  const [message, setMessage] = useState("");
+  const values = {
+    message,
+    convo_id: activeConversation._id,
+    files: [],
+    token,
+  };
+
+  const sendMessageHandler = async (e) => {
+    e.preventDefault();
+    await dispatch(sendMessage(values));
+    setMessage("");
+  };
   return (
-    <form className="dark:bg-dark_bg_2 h-[60px] w-full flex items-center absolute bottom-0 py-2 px-4 select-none">
+    <form
+      onSubmit={(e) => sendMessageHandler(e)}
+      className="dark:bg-dark_bg_2 h-[60px] w-full flex items-center absolute bottom-0 py-2 px-4 select-none"
+    >
       {/* Container */}
       <div className="w-full flex items-center gap-x-2">
         {/* Emojis and attachment */}
@@ -14,10 +38,14 @@ function ChatActions() {
           <Attachments></Attachments>
         </ul>
         {/* Input */}
-        <Input></Input>
+        <Input message={message} setMessage={setMessage}></Input>
         {/* Send Button */}
-        <button className="btn">
-          <SendIcon className="dark:fill-dark_svg_1"></SendIcon>
+        <button type="submit" className="btn">
+          {status === "loading" ? (
+            <ClipLoader color="#E9EDEF" size={25} />
+          ) : (
+            <SendIcon className="dark:fill-dark_svg_1" />
+          )}
         </button>
       </div>
     </form>
--- frontend/src/features/chatSlice.js
+++ frontend/src/features/chatSlice.js
@@ -72,6 +72,34 @@ export const getConversationMessages = createAsyncThunk(
   }
 );
 
+//functions
+export const sendMessage = createAsyncThunk(
+  "message/send",
+  async (values, { rejectWithValue }) => {
+    const { token, message, convo_id, files } = values;
+    try {
+      const { data } = await axios.post(
+        `${MESSAGE_ENDPOINT}`,
+        //sending to the body
+        {
+          message,
+          convo_id,
+          files,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data.error.message);
+    }
+  }
+);
+
 export const chatSlice = createSlice({
   name: "chat",
   initialState,
@@ -113,6 +141,17 @@ export const chatSlice = createSlice({
         state.messages = action.payload;
       })
       .addCase(getConversationMessages.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(sendMessage.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(sendMessage.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.messages = [...state.messages, action.payload];
+      })
+      .addCase(sendMessage.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.payload;
       });
